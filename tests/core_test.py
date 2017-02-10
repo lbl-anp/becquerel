94ad1c6081730e6178f997ea339469a490a4751c
Brian Plimley <as.white.as.snow@gmail.com>
2017-02-09 23:18:10 -0800

    update module init and core_test for Spectrum

--- tests/core_test.py
+++ tests/core_test.py
@@ -1,4 +1,4 @@
-"""Test becquerel's RawSpectrum and CalSpectrum."""
+"""Test becquerel's Spectrum."""
 
 from __future__ import print_function
 import unittest
@@ -7,31 +7,8 @@ import becquerel as bq
 from parsers_test import SAMPLES
 
 
-class RawSpectrumFromFileTests(unittest.TestCase):
-    """Test RawSpectrum.from_file() class method."""
-
-    def run_from_file(self, extension):
-        """Run the test of from_file() for files with the given extension."""
-        filenames = SAMPLES.get(extension, [])
-        self.assertTrue(len(filenames) >= 1)
-        for filename in filenames:
-            spec = bq.core.RawSpectrum.from_file(filename)
-
-    def test_spe(self):
-        """Test RawSpectrum.from_file for SPE file........................."""
-        self.run_from_file('.spe')
-
-    def test_spc(self):
-        """Test RawSpectrum.from_file for SPC file........................."""
-        self.run_from_file('.spc')
-
-    def test_cnf(self):
-        """Test RawSpectrum.from_file for CNF file........................."""
-        self.run_from_file('.cnf')
-
-
-class CalSpectrumFromFileTests(unittest.TestCase):
-    """Test CalSpectrum.from_file() class method."""
+class SpectrumFromFileTests(unittest.TestCase):
+    """Test Spectrum.from_file() class method."""
 
     def run_from_file(self, extension):
         """Run the test of from_file() for files with the given extension."""
@@ -41,15 +18,15 @@ class CalSpectrumFromFileTests(unittest.TestCase):
             spec = bq.core.CalSpectrum.from_file(filename)
 
     def test_spe(self):
-        """Test CalSpectrum.from_file for SPE file........................."""
+        """Test Spectrum.from_file for SPE file........................."""
         self.run_from_file('.spe')
 
     def test_spc(self):
-        """Test CalSpectrum.from_file for SPC file........................."""
+        """Test Spectrum.from_file for SPC file........................."""
         self.run_from_file('.spc')
 
     def test_cnf(self):
-        """Test CalSpectrum.from_file for CNF file........................."""
+        """Test Spectrum.from_file for CNF file........................."""
         self.run_from_file('.cnf')
 
 
