c86fd5cdfa50e36b04081771aa532af0495bf000
markbandstra <msbandstra@lbl.gov>
2017-03-05 23:21:39 -0800

    Remove element symbols above Z=100

--- becquerel/tools/xcom.py
+++ becquerel/tools/xcom.py
@@ -110,8 +110,7 @@ ELEMENT_SYMBOLS = [
     'Pm', 'Sm', 'Eu', 'Gd', 'Tb', 'Dy', 'Ho', 'Er', 'Tm', 'Yb', 'Lu', 'Hf',
     'Ta', 'W', 'Re', 'Os', 'Ir', 'Pt', 'Au', 'Hg', 'Tl', 'Pb', 'Bi', 'Po',
     'At', 'Rn', 'Fr', 'Ra', 'Ac', 'Th', 'Pa', 'U', 'Np', 'Pu', 'Am', 'Cm',
-    'Bk', 'Cf', 'Es', 'Fm', 'Md', 'No', 'Lr', 'Rf', 'Db', 'Sg', 'Bh', 'Hs',
-    'Mt', 'Ds', 'Rg', 'Cn', 'Nh', 'Fl', 'Mc', 'Lv', 'Ts', 'Og']
+    'Bk', 'Cf', 'Es', 'Fm']
 
 
 class XCOMError(Exception):
