d14b9d15e085c9e217dd62dabd57d899355c2ea1
Brian Plimley <as.white.as.snow@gmail.com>
2017-04-13 13:30:35 -0700

    python3 bug in becquerel/__init__.py

--- becquerel/__init__.py
+++ becquerel/__init__.py
@@ -4,7 +4,7 @@ from . import core
 from . import parsers
 from . import tools
 
-from core import (Spectrum, SpectrumError, UncalibratedError,
+from .core import (Spectrum, SpectrumError, UncalibratedError,
                   LinearEnergyCal, EnergyCalError, BadInput)
 
 __all__ = ['core', 'parsers', 'tools',
