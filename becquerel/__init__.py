9cd71cdff2cb7c320401486f44f3159ce4428263
Joseph Curtis <joseph.c.curtis@gmail.com>
2017-11-28 12:46:43 -0800

    Added rebin to bq namespace

--- becquerel/__init__.py
+++ becquerel/__init__.py
@@ -4,11 +4,12 @@ from . import core
 from . import parsers
 from . import tools
 
+from .core.rebin import rebin
 from .core.spectrum import Spectrum, SpectrumError, UncalibratedError
 from .core.energycal import LinearEnergyCal, EnergyCalError, BadInput
 from .core.utils import UncertaintiesError
 
-__all__ = ['core', 'parsers', 'tools',
+__all__ = ['core', 'parsers', 'tools', 'rebin',
            'Spectrum', 'SpectrumError', 'UncalibratedError',
            'LinearEnergyCal', 'EnergyCalError', 'BadInput',
            'UncertaintiesError']
