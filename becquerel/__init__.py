1c4e4d0b9bf5cdd4e4c53bc0cf66ef29b4218b1c
Joseph Curtis <jccurtis@lbl.gov>
2018-04-06 15:36:19 -0700

    Added tools to bq top level imports

--- becquerel/__init__.py
+++ becquerel/__init__.py
@@ -9,6 +9,13 @@ from .core.energycal import LinearEnergyCal, EnergyCalError, BadInput
 from .core.utils import UncertaintiesError
 from .core.plotting import SpectrumPlotter, PlottingError
 
+from .tools import nndc
+from .tools.element import Element
+from .tools.isotope import Isotope
+from .tools.isotope_qty import IsotopeQuantity
+from .tools import xcom
+from .tools import materials
+
 __all__ = ['core', 'parsers', 'tools',
            'Spectrum', 'SpectrumError', 'SpectrumPlotter', 'PlottingError',
            'UncalibratedError', 'LinearEnergyCal', 'EnergyCalError',
