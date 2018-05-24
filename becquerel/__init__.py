b4a26d0da43040383f0adfbe28ea497fda3b001e
cosama <marco.salathe@gmail.com>
2018-05-24 10:43:37 -0700

    Added methods to draw errorbands and errorbars, added most of the required tests and fixed quite a bit of bugs

--- becquerel/__init__.py
+++ becquerel/__init__.py
@@ -7,7 +7,7 @@ from . import tools
 from .core.spectrum import Spectrum, SpectrumError, UncalibratedError
 from .core.energycal import LinearEnergyCal, EnergyCalError, BadInput
 from .core.utils import UncertaintiesError
-from .core.plotting import SpectrumPlotter
+from .core.plotting import SpectrumPlotter, PlottingError
 
 __all__ = ['core', 'parsers', 'tools',
            'Spectrum', 'SpectrumError', 'SpectrumPlotter', 'UncalibratedError',
