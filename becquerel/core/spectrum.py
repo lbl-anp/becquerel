fix some off-by-one errors, update spectrum.ipynb

becquerel/core/spectrum.py
@@ -782,12 +782,20 @@ class Spectrum(object):
                         bin_edges_kev=self.bin_edges_kev,
                         livetime=new_livetime)
 
-    # possibly the best way to test for uniform bins within float precision
-    # could set a flag at creation (that would have to be able to change with a
-    # non-linear calibration) and use this as a test
-    # this could also use self.bin_widths being defined even without calibration
-    # https://github.com/0cjs/py-allthesame
     def has_uniform_bins(self):
+        """Test whether the Spectrum has uniform binning.
+
+        This is possibly the best way to test for uniform binning within float
+        precision, after https://github.com/0cjs/py-allthesame. It may also be
+        useful to make this a member, not a method, that gets set at Spectrum
+        initialization.
+
+        TODO: this calls self.bin_widths, which is undefined if not calibrated,
+        but this should work regardless.
+
+        Returns:
+          True/False if all binwidths are equal to within np.finfo(float).eps
+        """
         iterator = iter(self.bin_widths)
         x0 = next(iterator, None)
         for x in iterator:
@@ -795,21 +803,44 @@ class Spectrum(object):
                 return False
         return True
 
-    # FIXME JV added
-    # FIXME double-check math
-    # FIXME untested in new form
-    # FIXME needs to work with uncal channels, too
     def find_bin(self, x):
+        """Find the Spectrum bin that contains a value x.
+
+        If the Spectrum has uniform binning, then we just solve the linear
+        equation between bins and x-axis values. Otherwise, we use bisect_left
+        to solve for the insertion point where x would fit in a list of bin
+        edges, then subtract 1 to get the index of the low edge.
+
+        TODO: ensure this works with uncal channels, too
+
+        Args:
+          x: value whose bin to find
+
+        Raises:
+          AssertionError: if x is outside the bin edges
+
+        Returns:
+          The integer bin number that contains x
+        """
         assert x >= self.bin_edges_kev[0]
         assert x <= self.bin_edges_kev[-1]
 
         if self.has_uniform_bins():
             return int((x - self.bin_edges_kev[0]) / self.bin_widths[0])
         else:
-            return bisect_left(self.bin_edges_kev, x)
+            return bisect_left(self.bin_edges_kev, x) - 1
 
-    # this can simplify to return len(self.bin_widths) if it works for uncal
     def get_nbins(self):
+        """Get the number of bins in the Spectrum.
+
+        Note that the rightmost value _is_ included in the bin_edges
+
+        TODO: simplify to return len(self.bin_widths) if they are defined for
+        uncalibrated spectra. Although, that recomputes the diff...
+
+        Returns:
+          The integer number of bins in the Spectrum
+        """
         if self.is_calibrated:
             return len(self.bin_edges_kev) - 1
         else: