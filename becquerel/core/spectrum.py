d68b2d6d87ceb2aa9c7ec8ea6f033c5971095837
Jayson Vavrek <jvavrek@lbl.gov>
2021-02-06 14:04:51 -0800

    store calibration

--- becquerel/core/spectrum.py
+++ becquerel/core/spectrum.py
@@ -140,6 +140,7 @@ class Spectrum(object):
         self._cps = None
         self._bin_edges_kev = None
         self._bin_edges_raw = None
+        self.cal = None
         self.livetime = None
         self.realtime = None
 
@@ -1034,6 +1035,7 @@ class Spectrum(object):
         """
 
         self.bin_edges_kev = cal.ch2kev(self.bin_edges_raw)
+        self.cal = cal
 
     def calibrate_like(self, other):
         """Apply another Spectrum object's calibration (bin edges vector).
@@ -1050,6 +1052,7 @@ class Spectrum(object):
 
         if other.is_calibrated:
             self.bin_edges_kev = other.bin_edges_kev.copy()
+            self.cal = other.cal
         else:
             raise UncalibratedError('Other spectrum is not calibrated')
 
@@ -1057,6 +1060,7 @@ class Spectrum(object):
         """Remove the calibration (if it exists) from this spectrum."""
 
         self.bin_edges_kev = None
+        self.cal = None
 
     def combine_bins(self, f):
         """Make a new Spectrum with counts combined into bigger bins.
