38a87d4e7f0aeba40d53c2b22c6c36a627aab8b7
Joseph Curtis <jccurtis@lbl.gov>
2021-03-04 19:42:12 -0800

    Update pypi classifiers

--- setup.py
+++ setup.py
@@ -30,6 +30,10 @@ License :: Other/Proprietary License
 Operating System :: OS Independent
 Programming Language :: Python
 Programming Language :: Python :: 3
+Programming Language :: Python :: 3 :: Only
+Programming Language :: Python :: 3.6
+Programming Language :: Python :: 3.7
+Programming Language :: Python :: 3.8
 Topic :: Scientific/Engineering
 Topic :: Scientific/Engineering :: Physics
 """
