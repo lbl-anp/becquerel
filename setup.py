47fc352cb2931e041d0da5fbf47c187c65be3c97
Joseph Curtis <jccurtis@lbl.gov>
2021-03-04 19:43:03 -0800

    Try python3.9

--- setup.py
+++ setup.py
@@ -34,6 +34,7 @@ Programming Language :: Python :: 3 :: Only
 Programming Language :: Python :: 3.6
 Programming Language :: Python :: 3.7
 Programming Language :: Python :: 3.8
+Programming Language :: Python :: 3.9
 Topic :: Scientific/Engineering
 Topic :: Scientific/Engineering :: Physics
 """
