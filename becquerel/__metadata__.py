6637dd65e76eea32fd93e3ef694957bfe3cd5c5e
Joseph Curtis <jccurtis@lbl.gov>
2021-03-31 13:20:42 -0700

    Remove spaces from copyright and clean up formating in LICENSE

--- becquerel/__metadata__.py
+++ becquerel/__metadata__.py
@@ -31,8 +31,8 @@ Dept. of Energy). All rights reserved. If you have questions about your rights
 to use or distribute this software, please contact Berkeley Lab's Innovation &
 Partnerships Office at  IPO@lbl.gov.
 
-NOTICE.  This Software was developed under funding from the U.S. Department of
-Energy and the U.S. Government consequently retains certain rights.  As such,
+NOTICE. This Software was developed under funding from the U.S. Department of
+Energy and the U.S. Government consequently retains certain rights. As such,
 the U.S. Government has been granted for itself and others acting on its
 behalf a paid-up, nonexclusive, irrevocable, worldwide license in the Software
 to reproduce, distribute copies to the public, prepare derivative works, and
