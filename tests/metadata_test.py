914d1393cdb2ed5636cf11d83a5d77145a23f789
Joseph Curtis <jccurtis@lbl.gov>
2021-04-28 13:04:33 -0700

    Add tests for copyright and version text consistency

--- tests/metadata_test.py
+++ tests/metadata_test.py
@@ -2,23 +2,45 @@
 from utils import REPO_PATH
 import becquerel as bq
 
-COPYRIGHT = (REPO_PATH / "COPYRIGHT.txt").read_text().strip("\n")
+COPYRIGHT = (REPO_PATH / "COPYRIGHT.txt").read_text()
+LICENSE = (REPO_PATH / "LICENSE.txt").read_text()
+README = (REPO_PATH / "README.md").read_text()
+COPYRIGHT_IN_LICENSE = (
+    LICENSE.split("*** Copyright Notice ***")[-1]
+    .split("*** License Agreement ***")[0]
+    .strip()
+    + "\n"
+)
 
 
-def test_copyright_in_license():
-    license = (REPO_PATH / "LICENSE.txt").read_text()
-    copyright_in_license = (
-        license.split("*** Copyright Notice ***")[-1]
-        .split("*** License Agreement ***")[0]
-        .strip("\n")
-    )
-    assert COPYRIGHT == copyright_in_license
+class TestCopyright:
+    def test_license(self):
+        assert COPYRIGHT == COPYRIGHT_IN_LICENSE
+
+    def test_readme(self):
+        assert README.endswith(COPYRIGHT)
+
+    def test_metadata(self):
+        assert bq.__metadata__.__copyright__ == COPYRIGHT
 
 
-def test_copyright_in_readme():
-    readme = (REPO_PATH / "README.md").read_text().strip("\n")
-    assert readme.endswith(COPYRIGHT)
+class TestVersion:
+    def test_copyright(self):
+        assert (
+            COPYRIGHT.split("Becquerel v. ")[1].split(",")[0]
+            == bq.__metadata__.__version__
+        )
 
+    def test_license(self):
+        assert (
+            LICENSE.split("Becquerel v. ")[1].split(",")[0]
+            == bq.__metadata__.__version__
+        )
 
-def test_copyright_in_metadata():
-    assert bq.__metadata__.__copyright__.strip("\n") == COPYRIGHT
+    def test_readme(self):
+        assert (
+            README.split("## Copyright Notice")[1]
+            .split("Becquerel v. ")[1]
+            .split(",")[0]
+            == bq.__metadata__.__version__
+        )
