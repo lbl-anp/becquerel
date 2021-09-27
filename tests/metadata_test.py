bdf4f266ccbc7bbcd84cee0babad1739482777fe
Joseph Curtis <jccurtis@lbl.gov>
2021-09-27 13:17:56 -0700

    Update copyright for LBNL OSS - 2021-164 and remove version testing since the version is no longer in the copyright or license.

--- tests/metadata_test.py
+++ tests/metadata_test.py
@@ -5,42 +5,14 @@ import becquerel as bq
 COPYRIGHT = (REPO_PATH / "COPYRIGHT.txt").read_text()
 LICENSE = (REPO_PATH / "LICENSE.txt").read_text()
 README = (REPO_PATH / "README.md").read_text()
-COPYRIGHT_IN_LICENSE = (
-    LICENSE.split("*** Copyright Notice ***")[-1]
-    .split("*** License Agreement ***")[0]
-    .strip()
-    + "\n"
-)
 
 
 class TestCopyright:
     def test_license(self):
-        assert COPYRIGHT == COPYRIGHT_IN_LICENSE
+        assert COPYRIGHT == LICENSE.split("\n\n")[1]
 
     def test_readme(self):
         assert README.endswith(COPYRIGHT)
 
     def test_metadata(self):
         assert bq.__metadata__.__copyright__ == COPYRIGHT
-
-
-class TestVersion:
-    def test_copyright(self):
-        assert (
-            COPYRIGHT.split("Becquerel v. ")[1].split(",")[0]
-            == bq.__metadata__.__version__
-        )
-
-    def test_license(self):
-        assert (
-            LICENSE.split("Becquerel v. ")[1].split(",")[0]
-            == bq.__metadata__.__version__
-        )
-
-    def test_readme(self):
-        assert (
-            README.split("## Copyright Notice")[1]
-            .split("Becquerel v. ")[1]
-            .split(",")[0]
-            == bq.__metadata__.__version__
-        )
