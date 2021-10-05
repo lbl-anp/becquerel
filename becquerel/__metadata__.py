38f238fa41bf3ddad37a5148f00d4a44f01a9dd8
Joseph Curtis <jccurtis@lbl.gov>
2021-10-05 11:31:14 -0700

    Bump version: 0.4.2 → 0.4.3

--- becquerel/__metadata__.py
+++ becquerel/__metadata__.py
@@ -6,7 +6,7 @@ __maintainer__ = __author__
 __email__ = "becquerel-dev@lbl.gov"
 __description__ = "Tools for radiation spectral analysis."
 __url__ = "https://github.com/lbl-anp/becquerel"
-__version__ = "0.4.2"
+__version__ = "0.4.3"
 # classifiers from list at https://pypi.org/classifiers/
 __classifiers__ = [
     "Development Status :: 4 - Beta",
