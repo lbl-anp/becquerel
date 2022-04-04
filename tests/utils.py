93398726a525c21777a039d393e9d604c4fe8dae
Jayson Vavrek <jvavrek@lbl.gov>
2022-04-04 16:26:57 -0700

    return False in database checks if a ConnectionError occurs

--- tests/utils.py
+++ tests/utils.py
@@ -8,17 +8,23 @@ REPO_PATH = TESTS_PATH.parent
 
 def nndc_is_up():
     """Check whether the NNDC databases can be reached."""
-    return (
-        requests.post("https://www.nndc.bnl.gov/nudat3/indx_sigma.jsp").status_code
-        == requests.codes.ok
-    )
+    try:
+        return (
+            requests.post("https://www.nndc.bnl.gov/nudat3/indx_sigma.jsp").status_code
+            == requests.codes.ok
+        )
+    except requests.exceptions.ConnectionError:
+        return False
 
 
 def xcom_is_up():
     """Check whether the NIST XCOM databases can be reached."""
-    return (
-        requests.post(
-            "https://physics.nist.gov/PhysRefData/Xcom/html/xcom1.html"
-        ).status_code
-        == requests.codes.ok
-    )
+    try:
+        return (
+            requests.post(
+                "https://physics.nist.gov/PhysRefData/Xcom/html/xcom1.html"
+            ).status_code
+            == requests.codes.ok
+        )
+    except requests.exceptions.ConnectionError:
+        return False
