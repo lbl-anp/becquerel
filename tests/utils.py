f430676e02c8b8f6a6ca2f744320e36b4bd90d15
Jayson Vavrek <jvavrek@lbl.gov>
2022-04-04 15:48:48 -0700

    add nndc_is_up() util

--- tests/utils.py
+++ tests/utils.py
@@ -1,5 +1,14 @@
 """Helpers for testing."""
 from pathlib import Path
+import requests
 
 TESTS_PATH = Path(__file__).parent.absolute()
 REPO_PATH = TESTS_PATH.parent
+
+
+def nndc_is_up():
+    """Check whether the NNDC databases can be reached."""
+    return (
+        requests.post("https://www.nndc.bnl.gov/nudat3/indx_sigma.jsp").status_code
+        == requests.codes.ok
+    )
