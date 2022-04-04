486fc18166ebf5245b86d0dc361b1a86ca513902
Jayson Vavrek <jvavrek@lbl.gov>
2022-04-04 16:29:40 -0700

    dry refactor database_is_up

--- tests/utils.py
+++ tests/utils.py
@@ -6,25 +6,25 @@ TESTS_PATH = Path(__file__).parent.absolute()
 REPO_PATH = TESTS_PATH.parent
 
 
-def nndc_is_up():
-    """Check whether the NNDC databases can be reached."""
+def database_is_up(url):
+    """Check whether an online database can be reached.
+
+    Parameters
+    ----------
+    url : str
+        URL to query.
+    """
     try:
-        return (
-            requests.post("https://www.nndc.bnl.gov/nudat3/indx_sigma.jsp").status_code
-            == requests.codes.ok
-        )
+        return requests.post(url).status_code == requests.codes.ok
     except requests.exceptions.ConnectionError:
         return False
+
+
+def nndc_is_up():
+    """Check whether the NNDC databases can be reached."""
+    return database_is_up("https://www.nndc.bnl.gov/nudat3/indx_sigma.jsp")
 
 
 def xcom_is_up():
     """Check whether the NIST XCOM databases can be reached."""
-    try:
-        return (
-            requests.post(
-                "https://physics.nist.gov/PhysRefData/Xcom/html/xcom1.html"
-            ).status_code
-            == requests.codes.ok
-        )
-    except requests.exceptions.ConnectionError:
-        return False
+    return database_is_up("https://physics.nist.gov/PhysRefData/Xcom/html/xcom1.html")
