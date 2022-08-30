f957878cb483ae5b94c6cb898c2e505e1cb3d4d5
Jayson Vavrek <jvavrek@lbl.gov>
2022-08-30 16:01:31 -0700

    add param name checking to fitting tests

--- tests/fitting_test.py
+++ tests/fitting_test.py
@@ -291,6 +291,14 @@ class TestFittingHighStatSimData:
         assert bq.fitting._is_count_like(fitter.y_roi)
         assert not bq.fitting._is_count_like(fitter.y_roi * 0.5)
 
+        name0 = fitter.param_names[0]
+        fitter.param_val(name0)
+        fitter.param_unc(name0)
+        with pytest.raises(bq.core.fitting.FittingError):
+            fitter.param_val("bad_name")
+        with pytest.raises(bq.core.fitting.FittingError):
+            fitter.param_unc("bad_name")
+
     @pytest.mark.filterwarnings("ignore")
     def test_no_roi(self, sim_high_stat):
         fitter = bq.Fitter(sim_high_stat["model"])
