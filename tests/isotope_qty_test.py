16b80632c0dfcc9f1cae9da0cd9aaefdcbacacd0
Jayson Vavrek <jvavrek@lbl.gov>
2023-06-29 22:40:31 -0700

    delete test that will no longer error

--- tests/isotope_qty_test.py
+++ tests/isotope_qty_test.py
@@ -184,7 +184,6 @@ def test_handle_datetime(date_in):
     "iso, date, kwargs, error",
     [
         (["Cs-137"], None, {"atoms": 1e24}, TypeError),
-        ("Cs-137", 123, {"bq": 456}, TypeError),
         ("Cs-137", datetime.datetime.now(), {"asdf": 3}, IsotopeQuantityError),
         ("Cs-137", None, {"bq": -13.3}, ValueError),
     ],
