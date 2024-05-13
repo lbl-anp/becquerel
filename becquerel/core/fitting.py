f1e9eb8d8a981edbf9b959d9dc571510f1f75f12
Jayson Vavrek <jvavrek@lbl.gov>
2024-05-13 15:08:10 -0700

    only return uncertainties on parameters that can vary

--- becquerel/core/fitting.py
+++ becquerel/core/fitting.py
@@ -1128,9 +1128,13 @@ class Fitter:
         raise FittingError(f"Unknown backend: {self.backend}")
 
     @property
-    def uncertainties(self) -> dict[str: float]:
+    def uncertainties(self) -> dict[str:float]:
         """Dictionary of uncertainties, keyed on param name."""
-        return {param: self.param_unc(param) for param in self.param_names}
+        return {
+            param: self.param_unc(param)
+            for param in self.params
+            if self.params[param].vary
+        }
 
     @property
     def success(self):
