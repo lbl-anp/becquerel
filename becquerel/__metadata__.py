1bf7174981dde944a09ead4e4e3762e82c9bff52
Mark Bandstra <msbandstra@lbl.gov>
2025-01-29 12:48:13 -0800

    Add formal support for python3.13

--- becquerel/__metadata__.py
+++ becquerel/__metadata__.py
@@ -19,6 +19,7 @@ __classifiers__ = [
     "Programming Language :: Python :: 3.10",
     "Programming Language :: Python :: 3.11",
     "Programming Language :: Python :: 3.12",
+    "Programming Language :: Python :: 3.13",
     "Topic :: Scientific/Engineering",
     "Topic :: Scientific/Engineering :: Physics",
 ]
